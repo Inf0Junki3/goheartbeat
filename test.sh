6abc4bc907f001939975048bba2c1c9dcd083a25
jupyter <jupyter>
2021-05-08 15:07:50 +0000

    Added coverage information in the test.sh file.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 # This file performs the base tests for heartbeat. If something does not pass, first check that the 
 # endpoints are correct (e.g. that they're live. You can do this with curl, netcat or ping), then
 # check the code.
-go test *.go -v
+go test *.go -v -coverprofile=coverage.out
+go tool cover -html coverage.out -o coverage.html
